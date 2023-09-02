f75f336d17d301034a7195acca42d17c0a70c24e
25Huseyn <huseynajafov@gmail.com>
2023-09-02 14:09:31 +0400

    eolympArray

---
+++ src/main/java/az/atlacademy/eolympArray/Task1.java
@@ -0,0 +1,16 @@
+package az.atlacademy.eolympArray;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] nums = new int[3];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = scan.nextInt();
+        }
+        Arrays.sort(nums);
+        System.out.println(nums[1]);
+    }
+}
---
+++ src/main/java/az/atlacademy/eolympArray/Task4.java
@@ -0,0 +1,28 @@
+package az.atlacademy.eolympArray;
+
+
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double x = scan.nextDouble();
+        double y = scan.nextDouble();
+        double z = scan.nextDouble();
+        double result = min(max(x, y), max(y, z), x + y + z);
+        System.out.println(result);
+
+    }
+
+    public static double max(double x, double z) {
+        return Math.max(x, z);
+    }
+
+    public static double min(double x, double z) {
+        return Math.min(x, z);
+    }
+
+    public static double min(double x, double y, double z) {
+        return min(min(x, y), z);
+    }
+}
