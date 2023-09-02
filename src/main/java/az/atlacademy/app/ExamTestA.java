395c2d7148cb3effd0a0a579b4990622a2ed85c9
25Huseyn <huseynajafov@gmail.com>
2023-09-02 12:41:33 +0400

    Exam

---
+++ src/main/java/az/atlacademy/app/ExamTestA.java
@@ -0,0 +1,18 @@
+package az.atlacademy.app;
+
+import java.util.Scanner;
+
+public class ExamTestA {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double max = -1;
+        while (scan.hasNext()){
+            double salary = scan.nextDouble();
+
+            if (max < salary ){
+                max = (double) salary;
+            }
+
+        } System.out.printf("%.2f",max);
+    }
+}
