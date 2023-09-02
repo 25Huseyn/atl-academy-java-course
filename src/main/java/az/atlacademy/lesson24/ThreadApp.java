56754e5632463e16c2b648e843401f43146aa0f3
25Huseyn <huseynajafov@gmail.com>
2023-09-02 14:19:33 +0400

    MultiThreading

---
+++ src/main/java/az/atlacademy/lesson24/ThreadApp.java
@@ -0,0 +1,8 @@
+package az.atlacademy.lesson24;
+
+public class ThreadApp {
+    public static void main(String[] args) {
+        new Multithreading().start();
+        new Multithreading().run();
+    }
+}
